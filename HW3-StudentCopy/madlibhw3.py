3fe7176ea41498902feccb442d974b47abc331ba
Juliana Lam <julam@umich.edu>
2016-10-26 20:32:08 -0400

    Added keys

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -9,6 +9,13 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
+
+
+access_token = "791422914583298050-m0XgfqVnPO7hJSH2Oe37nuNQxLNnu6X"
+access_token_secret = "8dpUtkGfsRJNdeUl3Ya140EPWYx8tG7mzbArAlD7TIPED"
+consumer_key = "mHkUkEadhj0vb6lJoNRUNem2A"
+consumer_secret = "LIoMZWcPvOtx2E8mrX36ZxlaNWDvTuLyPJZn2MYPlv70wekd7Z"
+
 print("START*******")
 
 
--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,10 @@
 
 # You will demo this live for grading.
 
+access_token = "791422914583298050-m0XgfqVnPO7hJSH2Oe37nuNQxLNnu6X"
+access_token_secret = "8dpUtkGfsRJNdeUl3Ya140EPWYx8tG7mzbArAlD7TIPED"
+consumer_key = "mHkUkEadhj0vb6lJoNRUNem2A"
+consumer_secret = "LIoMZWcPvOtx2E8mrX36ZxlaNWDvTuLyPJZn2MYPlv70wekd7Z"
+
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")

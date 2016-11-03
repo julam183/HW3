a07c597e0a3b5464e4cf473bc74f2d5bbadbbb18
Juliana Lam <julam@umich.edu>
2016-11-02 22:22:45 -0400

    twitter hw 1

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,10 +4,32 @@
 
 # You will demo this live for grading.
 
-access_token = "791422914583298050-m0XgfqVnPO7hJSH2Oe37nuNQxLNnu6X"
-access_token_secret = "8dpUtkGfsRJNdeUl3Ya140EPWYx8tG7mzbArAlD7TIPED"
-consumer_key = "mHkUkEadhj0vb6lJoNRUNem2A"
-consumer_secret = "LIoMZWcPvOtx2E8mrX36ZxlaNWDvTuLyPJZn2MYPlv70wekd7Z"
+import tweepy
+import requests
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+def get_api(cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
+
+def tweet(image, message):
+    cfg = {
+        "consumer_key" : "mHkUkEadhj0vb6lJoNRUNem2A",
+        "consumer_secret" : "LIoMZWcPvOtx2E8mrX36ZxlaNWDvTuLyPJZn2MYPlv70wekd7Z",
+        "access_token" : "791422914583298050-m0XgfqVnPO7hJSH2Oe37nuNQxLNnu6X",
+        "access_token_secret" : "8dpUtkGfsRJNdeUl3Ya140EPWYx8tG7mzbArAlD7TIPED"
+    }
+
+    api = get_api(cfg)
+    img = ''
+    requests = requests.get(image, stream = True)
+    if requests.status_code == 200:
+        with open(img, 'wb') as image:
+            for chunk in request:
+                image.write(chunk)
+    api.update_with_media(img, status = message)
+    print("success")
+
+url = "https://s-media-cache-ak0.pinimg.com/564x/3c/d2/a8/3cd2a844037b921028481f9f3f82d21f.jpg"
+message = "#UMSI-206 #Proj3"
+tweet(image, message)
